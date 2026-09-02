443121bfe59bf9307272b0cc345ce8ff6463271b
Valeria Leskina <valeria.leskina@tptlive.ee>
2026-09-02 13:55:23 +0300

    Add types example

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
-echo "Hello, World!";// kirjutab Hello World browserisse
-
-$name = "John Doe "; //variable to store the name
-$age = 30; // Variable to  store the age
-echo $age;
-$age = $age * 2 * 3 * 4 * 5 * 6 * 7 * 8 * 9 * 10; // Update the age variable by multiplying it by a series
-echo $age; // Output the updated age
-
-echo 2 + 2; // Output the result of 2 + 2
-?>
+$test = 10;
+$test = 3.1;
+$test = "õpilane";
+$test = "😍";
+$test = true;
+$test = false;
+$test = null;
+$lol;
+var_dump($test);
+var_dump($lol);
+$test = [1, 2, "three", true];
+var_dump($test);
+$test = new stdClass();
+$test->name = "John";
+var_dump($test);
+$num1 = 10;
+$num2 = "10";
+var_dump($num1 === $num2);
