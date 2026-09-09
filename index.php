39a19f9d738aba472b43980a7103885776954fea
Valeria Leskina <valeria.leskina@tptlive.ee>
2026-09-09 09:33:52 +0300

    add fumctions

--- index.php
+++ index.php
@@ -1,42 +1,56 @@
 <?php
  
-for($i=0; $i<10; $i++){
-    var_dump($i);
+function hello() {
+    var_dump('Hello');
 }
  
-for($i=9; $i>=0; $i--){
-    var_dump($i);
+$singleHello = function () {
+    var_dump('Hello');
+};
+hello();
+hello();
+ 
+function helloName($name='Nameless') {
+    var_dump("Hello, $name!");
 }
  
-for($i=2; $i<1_000_000; $i*=2){
-    var_dump($i);
+helloName('Alissia');
+helloName('Valeria');
+ 
+function helloNameAndAge($name, $age) {
+    var_dump("Hello, $name! Youa are $age years old");
 }
  
-$time = time();
-$n = 0;
-while($time+1 > time()) {
-    $n++;
+helloNameAndAge('Alissia', 18);
+helloNameAndAge('Anna', 20);
+ 
+helloName();
+
+$test = function () {
+
 }
-var_dump($n);
- 
-while(false){
-    var_dump('WHILE');
+var_dump($test);
+
+
+$numbers = [1, 2, 3, 4, 5];
+
+$squares = array_map(function ($number) {
+    return $number * 2;
+}, $numbers);
+$squares = array_map(fn($number) => $number * $number, $numbers);
+var_dump($squares);
+
+function cube($a) {
+    if ($a < 0) {
+        return -$a * $a * $a;
+    }
+    return $a * $a * $a;
+    var_dump('This will never be executed');
 }
- 
-do{
-    var_dump('DO');
-} while(false);
- 
-// while(true) {
- 
-// }
- 
-$fruits = ['Apple', 'Cherry', 'Banana'];
- 
-foreach($fruits as $fruit) {
-    var_dump($fruit);
-}
- 
-foreach($fruits as $key=>$fruit) {
-    var_dump($fruit, $key);
-}
+
+var_dump(cube(4));
+
+$answer = cube(5);
+$text = "cube of 5 is $answer";
+echo $text;
+
