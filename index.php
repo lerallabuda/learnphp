8a956fb68beecd6bad64670467e12de9353c8bea
Valeria Leskina <valeria.leskina@tptlive.ee>
2026-09-09 09:04:05 +0300

    add loop examples

--- index.php
+++ index.php
@@ -1,40 +1,42 @@
 <?php
-$num = 10;
-if($num > 5) {
-    var_dump('Bigger');
+ 
+for($i=0; $i<10; $i++){
+    var_dump($i);
 }
-
-if($num > 5) {
-    var_dump('Bigger');
-} else {
-    var_dump('Smaller');
+ 
+for($i=9; $i>=0; $i--){
+    var_dump($i);
 }
-
-if($num > 5) {
-    var_dump('Bigger');
-} elseif($num == 5) {
-    var_dump('Equal');
-} else {
-    var_dump('Smaller');
+ 
+for($i=2; $i<1_000_000; $i*=2){
+    var_dump($i);
 }
-
-$i = 1;
-
-switch($i) {
-    case 1:
-        var_dump('xy');
-        break;
-    case 2:
-        var_dump('kaks');
-    case 3:
-    case 4:
-        var_dump('kolm või neli');
-        break;
-    case 5:
-        var_dump('viis');
-    case 6:
-        var_dump('kuus');
-        break;
-    default:
-        var_dump('dunno');
+ 
+$time = time();
+$n = 0;
+while($time+1 > time()) {
+    $n++;
+}
+var_dump($n);
+ 
+while(false){
+    var_dump('WHILE');
+}
+ 
+do{
+    var_dump('DO');
+} while(false);
+ 
+// while(true) {
+ 
+// }
+ 
+$fruits = ['Apple', 'Cherry', 'Banana'];
+ 
+foreach($fruits as $fruit) {
+    var_dump($fruit);
+}
+ 
+foreach($fruits as $key=>$fruit) {
+    var_dump($fruit, $key);
 }
