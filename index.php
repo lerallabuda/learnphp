c8989c9be856be78eb31e05cb226fdf096d5b53c
Valeria Leskina <valeria.leskina@tptlive.ee>
2026-09-11 15:01:05 +0300

    add php templateing example

--- index.php
+++ index.php
@@ -1,45 +1,28 @@
 <?php
+$name = 'Alissia';
+$coinFlip= rand(0,1);
+?>
  
-// library
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello <?=$name?>!</h1>
+    <?php if($coinFlip): ?>
+        <h1>Kull</h1>
+    <?php else: ?>
+        <h1>Kiri</h1>
+    <?php endif ?>
  
-class Job {
-    public function task(Logger $logger) {
-        for($i=0;$i<10;$i++){
-            //some work is done
-            $logger->log("Task $i was done!");
-        }
-    }
-}
+    <ul>
+        <?php for($i=0;$i<10;$i++): ?>
+            <li><?=$i?></li>
+        <?php endfor ?>
+    </ul>
+</body>
+</html>
  
-class ConsoleLogger implements Logger {
-    public function log($message) {
-        echo "$message\n";
-    }
-}
- 
-class NothingLogger {
-    public function log($message) {
- 
-    }
-}
- 
-interface Logger {
-    public function log($message);
-}
- 
- 
-// user code
- 
-class FileLogger implements Logger {
-    public function log($message) {
-        $file = fopen('log.txt', 'a');
-        fwrite($file, "$message\n");
-        fclose($file);
-    }
-}
- 
- 
- 
-$job = new Job();
-$logger = new ConsoleLogger();
-$job->task($logger);
