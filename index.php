9ba3fa78593d4620c169ac56f98710950540b066
Valeria Leskina <valeria.leskina@tptlive.ee>
2026-09-11 14:06:27 +0300

    add magic methods example

--- index.php
+++ index.php
@@ -1,23 +1,54 @@
 <?php
  
-class Cat {
-    use MakesSound;
-}
+class Box {
  
-class Dog {
-    use HasSmell, MakesSound;
-}
+    public function __construct(private $w, private $h, private $l) {
+        var_dump('Box was created!');
+    }
  
-trait hasSmell {
-    public $smell;
-    public function sniff() {
-        return 'Smells like' . $this->smell;
+    public function volume() {
+        return $this->w * $this->h * $this->l;
+    }
+ 
+    public function __set($name, $value) {
+        var_dump($name, $value);
+    }
+ 
+    public function __get() {
+        var_dump($name);
+        return 'YOLO';
+    }
+ 
+    public function __invoke() {
+        var_dump('Im a function');
+    }
+ 
+    public function __call($name, $args) {
+        var_dump($name, $args);
+    }
+ 
+    public function __toString() {
+        return "Im a box with W: $this->w H: $this->h L: $this->l";
+    }
+ 
+     public function __destruct() {
+        var_dump('Box was destroyed!');
     }
 }
  
-trait MakesSound {
-    public $sound;
-    public function noise() {
-        return $this->sound;
-    }
+function lol(){
+    $box4 = new Box(4, 4, 5);
 }
+ 
+lol();
+ 
+$box1 = new Box(1,2,3);
+$box1->color = 'hello';
+$box1();
+$box1->hello(1, 'asdasd', 'dsaad');
+var_dump($box1->lololol);
+$box1 = 1;
+$box2 = new Box(4,5,6);
+var_dump($box1, $box2);
+$box3 = clone $box2;
+echo $box1;
