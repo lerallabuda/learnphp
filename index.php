8eb2540793341840c31bcb30795049d78aa27b5f
Valeria Leskina <valeria.leskina@tptlive.ee>
2026-09-11 14:13:39 +0300

    add interface example

--- index.php
+++ index.php
@@ -1,27 +1,45 @@
 <?php
  
-class Box {
-    public static $count = 0;
-    public function __construct(private $w, private $h, private $l) {
-        self::$count;
-    }
+// library
  
-    public function volume() {
-        return $this->w * $this->h * $this->l;
-    }
- 
-    public static function me() {
-        var_dump(self::class);
-        var_dump(static::class);
+class Job {
+    public function task(Logger $logger) {
+        for($i=0;$i<10;$i++){
+            //some work is done
+            $logger->log("Task $i was done!");
+        }
     }
 }
  
-class MetalBox extends Box {
- 
+class ConsoleLogger implements Logger {
+    public function log($message) {
+        echo "$message\n";
+    }
 }
  
-Box::$count = 1;
-Box::$count = 2;
-Box::me();
-MetalBox::me();
-var_dump(Box::$count, Box::$count);
+class NothingLogger {
+    public function log($message) {
+ 
+    }
+}
+ 
+interface Logger {
+    public function log($message);
+}
+ 
+ 
+// user code
+ 
+class FileLogger implements Logger {
+    public function log($message) {
+        $file = fopen('log.txt', 'a');
+        fwrite($file, "$message\n");
+        fclose($file);
+    }
+}
+ 
+ 
+ 
+$job = new Job();
+$logger = new ConsoleLogger();
+$job->task($logger);
