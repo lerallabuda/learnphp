7c92b1a9ee2d2306558958fad85ea0af50267cfd
Valeria Leskina <valeria.leskina@tptlive.ee>
2026-09-14 10:05:43 +0300

    split blog template into partials

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
-<?php
-
-$sum = 0;
-
-for($i=1;$i<1000;$i++) {
-   if($i%3 === 0 || $i%5 === 0) {
-        $sum+=$i;
-   } 
-}
-var_dump($sum);
+<?php include __DIR__ . '/partials/header.php'; ?>
+   
+   
+    <main class="container">
+      <?php include __DIR__ . '/partials/hero.php'; ?>
+      <?php include __DIR__ . '/partials/featured.php'; ?>
+     
+      <div class="row g-5">
+        <div class="col-md-8">
+          <?php include __DIR__ . '/partials/posts.php'; ?>
+        </div>
+        <div class="col-md-4">
+          <?php include __DIR__ . '/partials/sidebar.php'; ?>
+        </div>
+      </div>
+    </main>
+  <?php include __DIR__ . '/partials/footer.php'; ?>  
+ 
