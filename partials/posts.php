9e6d8bcde00c0be6ffa58f7aca90a49bc2785548
Valeria Leskina <valeria.leskina@tptlive.ee>
2026-09-14 15:24:31 +0300

    Add U.S page and posts and title

--- partials/posts.php
+++ partials/posts.php
@@ -1,84 +1,21 @@
-<div class="row mb-2">
-        <div class="col-md-6">
-          <div
-            class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative"
-          >
-            <div class="col p-4 d-flex flex-column position-static">
-              <strong class="d-inline-block mb-2 text-primary-emphasis"
-                >World</strong
-              >
-              <h3 class="mb-0">Featured post</h3>
-              <div class="mb-1 text-body-secondary">Nov 12</div>
-              <p class="card-text mb-auto">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content.
-              </p>
-              <a
-                href="#"
-                class="icon-link gap-1 icon-link-hover stretched-link"
-              >
-                Continue reading
-                <svg class="bi" aria-hidden="true">
-                  <use xlink:href="#chevron-right"></use>
-                </svg>
-              </a>
-            </div>
-            <div class="col-auto d-none d-lg-block">
-              <svg
-                aria-label="Placeholder: Thumbnail"
-                class="bd-placeholder-img"
-                height="250"
-                preserveAspectRatio="xMidYMid slice"
-                role="img"
-                width="200"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c"></rect>
-                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-              </svg>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6">
-          <div
-            class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative"
-          >
-            <div class="col p-4 d-flex flex-column position-static">
-              <strong class="d-inline-block mb-2 text-success-emphasis"
-                >Design</strong
-              >
-              <h3 class="mb-0">Post title</h3>
-              <div class="mb-1 text-body-secondary">Nov 11</div>
-              <p class="mb-auto">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content.
-              </p>
-              <a
-                href="#"
-                class="icon-link gap-1 icon-link-hover stretched-link"
-              >
-                Continue reading
-                <svg class="bi" aria-hidden="true">
-                  <use xlink:href="#chevron-right"></use>
-                </svg>
-              </a>
-            </div>
-            <div class="col-auto d-none d-lg-block">
-              <svg
-                aria-label="Placeholder: Thumbnail"
-                class="bd-placeholder-img"
-                height="250"
-                preserveAspectRatio="xMidYMid slice"
-                role="img"
-                width="200"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c"></rect>
-                <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-              </svg>
-            </div>
-          </div>
-        </div>
-      </div>
+<h3 class="pb-4 mb-4 fst-italic border-bottom">From the Firehose</h3>
+        <?php foreach($posts as $post): ?>
+          <article class="blog-post">
+            <h2 class="display-5 link-body-emphasis mb-1"><?=  $post['title'] ?></h2>
+            <p class="blog-post-meta">
+              <?= $post['date'] ?> by <a href="#"><?=  $post['author'] ?></a>
+            </p>
+            <p>
+              <?= $post['content'] ?>
+            </p>
+          </article>
+        <?php endforeach ?>
+          <nav class="blog-pagination" aria-label="Pagination">
+            <a class="btn btn-outline-primary rounded-pill" href="#">Older</a>
+            <a
+              class="btn btn-outline-secondary rounded-pill disabled"
+              aria-disabled="true"
+              >Newer</a
+            >
+          </nav>
+ 
--- us.php
+++ us.php
@@ -1,5 +1,5 @@
 <?php
-
+$TITLE = 'U.S.';
 $posts = 
     [
         'title' => 'Same world title 1', 'content' => 'same world content 1', 'date' => 'January 1, 2021', 'author' => 'Pets',
@@ -28,18 +28,12 @@ $posts =
     ],
 
     ?>
-        
-
-
 
 
 <?php include __DIR__ . '/partials/header.php'; ?>
    
    
     <main class="container">
-      <?php include __DIR__ . '/partials/hero.php'; ?>
-      <?php include __DIR__ . '/partials/featured.php'; ?>
-     
       <div class="row g-5">
         <div class="col-md-8">
           <?php include __DIR__ . '/partials/posts.php'; ?>
