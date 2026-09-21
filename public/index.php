6573b3db6707dfdc50b18181e23b66ff49fe5d28
valeria leskina <valeria.leskina@tptlive.ee>
2026-09-21 09:10:47 +0300

    Add router logic

--- public/index.php
+++ public/index.php
@@ -15,14 +15,77 @@ spl_autoload_register(function ($class){
     require_once __DIR__ . "/../src/$class.php";
 });
 
-use App\Controllers\PublicController as PC;
+use App\Router;
 
-// require_once __DIR__ . '/../src/Router.php';
-// require_once __DIR__ . '/../src/DB.php';
-$controller = new PC();
-$router = new App\Router();
-$db = new App\DB();
-dump($router,$db);
+Router::addRoute('/', function () {
+        $title = 'World';
+        $posts = [
+            [
+                'title' => 'Some world title 1',
+                'content' => 'Some world content 1',
+                'date' => 'January 1, 2021',
+                'author' => 'Pets',
+            ],
+            [
+                'title' => 'Some world title 2',
+                'content' => 'Some world content 2',
+                'date' => 'January 2, 2021',
+                'author' => 'Juss',
+            ],
+            [
+                'title' => 'Some world title 3',
+                'content' => 'Some world content 3',
+                'date' => 'January 3, 2021',
+                'author' => 'Alex',
+            ],
+            [
+                'title' => 'Some world title 4',
+                'content' => 'Some world content 4',
+                'date' => 'January 4, 2021',
+                'author' => 'Manivald',
+            ],
+        ];
+        include __DIR__ . '/../views/index.php';
+});
+
+Router::addRoute('/us', function () {
+        $title = 'U.S';
+        $posts = [
+            [
+                'title' => 'Some U.S title 1',
+                'content' => 'Some U.S content 1',
+                'date' => 'January 1, 2021',
+                'author' => 'Pets',
+            ],
+            [
+                'title' => 'Some U.S title 2',
+                'content' => 'Some U.S content 2',
+                'date' => 'January 2, 2021',
+                'author' => 'Juss',
+            ],
+            [
+                'title' => 'Some U.S title 3',
+                'content' => 'Some U.S content 3',
+                'date' => 'January 3, 2021',
+                'author' => 'Alex',
+            ],
+            [
+                'title' => 'Some U.S title 4',
+                'content' => 'Some U.S content 4',
+                'date' => 'January 4, 2021',
+                'author' => 'Manivald',
+            ],
+        ];
+        include __DIR__ . '/../views/us.php';
+});
+
+$router = new Router($_SERVER['REQUEST_URI']);
+$match = $router->match();
+if($match) {
+    call_user_func($match['action']);
+} else {
+    echo 404;
+}
 
 // switch ($_SERVER['REQUEST_URI']) {
 //     case '/':
